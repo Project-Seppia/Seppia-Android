641f326ac717357d6647ed1cc7a4f20f7e82eec3
Zhang Qi <kksjtu@gmail.com>
2016-08-05 21:38:34 +0200

    some activities initially set up

---
+++ app/src/main/java/com/seppia/android/project_seppia/MainActivity.java
@@ -0,0 +1,13 @@
+package com.seppia.android.project_seppia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/seppia/android/project_seppia/settings/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.seppia.android.project_seppia.settings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.seppia.android.project_seppia.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
